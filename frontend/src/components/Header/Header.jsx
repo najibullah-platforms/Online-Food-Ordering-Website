fade26ac1f934afb11be2ac9b4bea1c8bfef7a9f
najibullah-platforms <najibullah.platforms@gmail.com>
2025-09-14 15:13:02 +0500

    first commit add navbar made it

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Header.css'
+const Header = () => {
+  return (
+    <div className='header'>
+      <div className="header-contents">
+        <h2>order you favourite food here</h2>
+        <p>Choose from a diverse menu Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet, magni harum!</p>
+        <button>View Menu</button>
+      </div>
+    </div>
+  )
+}
+
+export default Header
