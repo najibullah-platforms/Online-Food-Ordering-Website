8faedc5fb925d6c70940840e76ed4c4e396835b5
najibullah-platforms <najibullah.platforms@gmail.com>
2025-09-16 22:35:41 +0500

    frontedn code is don!

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -5,32 +5,39 @@ export const StoreContext = createContext(null);
 const StoreContextProvider = (props) => {
   // This is where you define values you want to share
 
-  const [cartItems,setCartItems] = useState({});
+  const [cartItems, setCartItems] = useState({});
 
   const addToCart = (itemId) => {
-    if(!cartItems[itemId]){
-      setCartItems((prev)=>({...prev,[itemId]:1}))
+    if (!cartItems[itemId]) {
+      setCartItems((prev) => ({ ...prev, [itemId]: 1 }));
+    } else {
+      setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
     }
-    else{
-      setCartItems((prev)=>({...prev,[itemId]:prev[itemId]+1}))
+  };
+
+  const removeFromCart = (itemId) => {
+    setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }));
+  };
+
+  const getTotalCartAmount = () => {
+    let totalAmount = 0;
+
+    for (const item in cartItems) {
+      if (cartItems[item] > 0) {
+        let itemInfo = food_list.find((product) => product._id === item);
+        totalAmount += itemInfo.price * cartItems[item];
+      }
     }
-  }
-
-
-  const removeFromCart = (itemId) =>{
-    setCartItems((prev)=>({...prev,[itemId]:prev[itemId]-1}))
-  }
-
-  useEffect(()=>{
-    console.log(cartItems)
-  },[cartItems])
+    return totalAmount;
+  };
 
   const contextValue = {
     food_list,
     cartItems,
     setCartItems,
     addToCart,
-    removeFromCart
+    removeFromCart,
+    getTotalCartAmount
   };
 
   return (
