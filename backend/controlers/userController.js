08fd5a363709ee7505a3782daba7187c73175321
najibullah-platforms <najibullah.platforms@gmail.com>
2025-09-18 06:40:40 +0500

    some file add for user

---
+++ backend/controlers/userController.js
@@ -0,0 +1,17 @@
+import userModel from "../moduls/userModul";
+import jwt from "jsonwebtoken"
+import bcrypt from 'bcrypt'
+import validator from "validator"
+
+// login user
+
+const loginUser = async (req,res) => {
+
+}
+
+//register uer
+const registerUser = async (req,res)=> {
+   
+}
+
+export {loginUser,registerUser}
--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express'
 import cors from 'cors'
 import { connectDB } from './config/db.js';
 import foodRouter from './routes/foodRoute.js';
+import userRouter from './routes/userRoute.js';
 
 // app config 
 const app = express()
@@ -18,6 +19,7 @@ connectDB();
 //api endpoinsts 
 app.use("/api/food",foodRouter)
 app.use("/image",express.static('uploads'))
+app.use("/api/user",userRouter)
 
 app.get("/",(req,res)=>{
     res.send("API Working")
