e8e8be2462b6180a233606c70761dd78f718e2d1
najibullah-platforms <najibullah.platforms@gmail.com>
2025-09-18 15:08:11 +0500

    my frondtend code is good

--- frontend/src/components/FoodItem/FoodItem.jsx
+++ frontend/src/components/FoodItem/FoodItem.jsx
@@ -5,11 +5,11 @@ import { StoreContext } from '../../context/StoreContext'
 const FoodItem = ({id,name,price,description,image}) => {
 
   const [itemCount,setItemCount] = useState(0)
-  const {cartItems,addToCart,removeFromCart} = useContext(StoreContext)
+  const {cartItems,addToCart,removeFromCart,url} = useContext(StoreContext)
   return (
     <div className='food-item'>
       <div className='food-item-img-container'>
-          <img src={image} className='food-item-image' alt="" />
+          <img src={url+"/images/"+image} className='food-item-image' alt="" />
           {!cartItems[id]
            ?<img className='add' onClick={()=>addToCart(id)} src={assets.add_icon_white} alt='' />
            :<div className='food-item-counter'>
