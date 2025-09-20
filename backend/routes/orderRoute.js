439a277865a6142ecd47e49916284559de8792d1
najibullah-platforms <najibullah.platforms@gmail.com>
2025-09-21 00:47:58 +0500

    backend finished

---
+++ backend/routes/orderRoute.js
@@ -0,0 +1,13 @@
+import express from "express"
+import authMiddleware from '../middleware/auth.js'
+import { placeOrder, verifyOrder,userOrders,listOrders,updateStatus } from "../controlers/orderController.js"
+
+const orderRouter = express.Router();
+
+orderRouter.post("/place",authMiddleware,placeOrder);
+orderRouter.post("/verify",verifyOrder)
+orderRouter.post("/userorders",authMiddleware,userOrders)
+orderRouter.get('/list',listOrders)
+orderRouter.get('/status',updateStatus)
+
+export default orderRouter;
--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import foodRouter from './routes/foodRoute.js';
 import userRouter from './routes/userRoute.js';
 import 'dotenv/config'
 import cartRouter from './routes/cartRoute.js';
+import orderRouter from './routes/orderRoute.js';
 // app config 
 const app = express()
 const port = 4000
@@ -22,6 +23,7 @@ app.use("/api/food",foodRouter)
 app.use("/image",express.static('uploads'))
 app.use("/api/user",userRouter)
 app.use("/api/cart",cartRouter)
+app.use("/api/order",orderRouter)
 
 app.get("/",(req,res)=>{
     res.send("API Working")
